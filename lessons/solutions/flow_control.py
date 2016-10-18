291c0c9d4d691c2ddc455338cb5c6ac4325547e4
Hassan Shamim <hassan.has.you@gmail.com>
2016-10-18 15:32:47 -0700

    finish up lesson3

--- lessons/solutions/flow_control.py
+++ lessons/solutions/flow_control.py
@@ -5,3 +5,16 @@ while countdown > 0:
     print(countdown)
     countdown = countdown - 1
     print('Blastoff!')
+
+# lesson 3 Bonus:
+from string import ascii_lowercase
+word = ''
+total = 0
+for char in word:
+    letter_value = 1
+    for letter in ascii_lowercase:
+        if letter == char:
+            total += letter_value
+            break
+        letter_value += 1
+print(total)
